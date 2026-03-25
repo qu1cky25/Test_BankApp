18923f665fceb7c5ccca7586852879af2d88e01d
qu1cky25 <qu1ck3r528@gmail.com>
2026-03-25 21:34:46 +0500

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBackend
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        public string Bank { get; set; }
+        public decimal Balance { get; set; }
+        public bool is_visible { get; set; } = true;
+        public List<Transaction> Transactions { get; set; } = new();
+    }
+}
